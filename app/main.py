9502f45a303c22823c88a44c8bd53a2be16068a4
daria425 <darianaumova5@gmail.com>
2025-02-05 13:31:32 +0200

    add mongo db connection

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from app.routes import events
-app=FastAPI()
+from app.db.database_connection import db_connection
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await db_connection.connect()
+    yield
+    await db_connection.close()
+
+app=FastAPI(lifespan=lifespan)
+
+
+
 
 @app.get("/")
 def root():
     return {"message":"Service is up!"}
+
+
 
 app.include_router(router=events.router, prefix="/events")
